246db72d150db8788e2d61f0370253cfdc38a42e
sk8forether <pzv2@cornell.edu>
2021-05-05 22:43:01 -0500

    Edits to paths and configure.wrf to (hopefully) fix compile issues (still testing)

--- Stampede2/Intel/config_compile_18.sh
+++ Stampede2/Intel/config_compile_18.sh
@@ -5,20 +5,20 @@
 module reset # Should include intel/18.0.2 impi/18.0.2
 module load netcdf/4.6.2 pnetcdf/1.11.0 phdf5/1.10.4
 
-export WRF_SRC_ROOT_DIR=~/WRF_Benchmarking/Docker-WRF-3.8.1-Fitch/Stampede2/Intel/WRFV3/
+export WRF_SRC_ROOT_DIR=~/WRF_Benchmarking/Docker-WRF-3.8.1-Fitch/Stampede2/Intel/WRFV3
 cd $WRF_SRC_ROOT_DIR
 
-export DIR=${WRF_SRC_ROOT_DIR}../LIBRARIES
+export LIBS=${WRF_SRC_ROOT_DIR}/../LIBRARIES
 export CC=icc
 export CXX=icpc
 export FC=ifort
 export FCFLAGS=-m64
 export F77=ifort
 export FFLAGS=-m64
-export JASPERLIB=$DIR/grib2/lib
-export JASPERINC=$DIR/grib2/include
-export LDFLAGS=-L$DIR/grib2/lib
-export CPPFLAGS=-I$DIR/grib2/include
+export JASPERLIB=$LIBS/grib2/lib
+export JASPERINC=$LIBS/grib2/include
+export LDFLAGS=-L$LIBS/grib2/lib
+export CPPFLAGS=-I$LIBS/grib2/include
 
 export PATH=${TACC_NETCDF_BIN}:${TACC_PNETCDF_BIN}:$PATH
 export NETCDF=${TACC_NETCDF_DIR}
@@ -45,16 +45,17 @@ echo '20\n1\n' | ./configure
 
 # modifications for Intel 
 sed -i -e 's/-openmp/-qopenmp/' ./configure.wrf
-sed -i -e 's/-lpnetcdf/-L${TACC_PNETCDF_LIB} -lnetcdff -lnetcdf /' ./configure.wrf
+#sed -i -e 's/-lpnetcdf/-L${TACC_PNETCDF_LIB} -lnetcdff -lnetcdf /' ./configure.wrf
 sed -i -e 's/gcc/icc/' ./configure.wrf
 
-# NetCDF linking needed for Stampede2
-read -r -d '' var<<"EOF"
--DNETCDF -DPNETCDF -I${WRF_SRC_ROOT_DIR}/external/io_netcdf -I${NETCDFPATH}/include -L${WRF_SRC_ROOT_DIR}external/io_netcdf -lwrfio_nf -L${TACC_NETCDF_LIB} -lnetcdff -lnetcdf -L${WRF_SRC_ROOT_DIR}external/io_pnetcdf -lwrfio_pnf -L${TACC_PNETCDF_LIB} -lpnetcdf
-EOF
-
+## NetCDF linking needed for Stampede2
+export var=" -DNETCDF -DPNETCDF -I${WRF_SRC_ROOT_DIR}/external/io_netcdf -I${NETCDF}/include -L${WRF_SRC_ROOT_DIR}/external/io_netcdf -lwrfio_nf -L${TACC_NETCDF_LIB} -lnetcdff -lnetcdf -I${WRF_SRC_ROOT_DIR}/external/io_pnetcdf -I${PNETCDF}/include -L${WRF_SRC_ROOT_DIR}/external/io_pnetcdf -lwrfio_pnf -L${TACC_PNETCDF_LIB} -lpnetcdf"
 #sed -i -e ''95's@$@'" $var"'@' configure.wrf
-awk -v mytext=" ${var}" '/^NETCDF4_DEP_LIB / {$0=$0mytext} 1' configure.wrf > temp && mv temp configure.wrf
+awk -v mytext="${var}" '/^NETCDF4_DEP_LIB / {$0=$0mytext} 1' configure.wrf > temp && mv temp configure.wrf
+
+awk -v add=" -L${LIBS}/zlib-1.2.7" '/^ZLIB/ {$0=$0add} 1' configure.wrf > temp && mv temp configure.wrf
+awk -v add="                      -I${WRF_SRC_ROOT_DIR}/external/io_pnetcdf \\" 'NR==200 {print add} {print}' configure.wrf > temp && mv temp configure.wrf
+awk -v add="                      -I${PNETCDF}/include \\" 'NR==201 {print add} {print}' configure.wrf > temp && mv temp configure.wrf
 
 # Compile
 #time ./compile -j 2 em_real >& compile_em_real.log
