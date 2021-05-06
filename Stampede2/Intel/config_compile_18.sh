888ee7e3d5670debad1aaf528636e41d23960d56
sk8forether <pzv2@cornell.edu>
2021-05-06 00:11:25 -0500

    Cleaning earlier to ensure MODULEPATH is correct, added some lib and include paths, forcing MODULEPATH to include dyn_nmm and dyn_em which keep failing to be found

--- Stampede2/Intel/config_compile_18.sh
+++ Stampede2/Intel/config_compile_18.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 # Script to run configure and compile with Intel 18 on Stampede2
 
+export WRF_SRC_ROOT_DIR=~/WRF_Benchmarking/Docker-WRF-3.8.1-Fitch/Stampede2/Intel/WRFV3
+cd $WRF_SRC_ROOT_DIR
+
+# Fresh config
+./clean -a
+
 # Load system modules
 module reset # Should include intel/18.0.2 impi/18.0.2
 module load netcdf/4.6.2 pnetcdf/1.11.0 phdf5/1.10.4
-
-export WRF_SRC_ROOT_DIR=~/WRF_Benchmarking/Docker-WRF-3.8.1-Fitch/Stampede2/Intel/WRFV3
-cd $WRF_SRC_ROOT_DIR
 
 export LIBS=${WRF_SRC_ROOT_DIR}/../LIBRARIES
 export CC=icc
@@ -37,25 +40,26 @@ cp ../../../patches/wrf_bug_fixes/Registry.EM_COMMON ./Registry/
 cp ../../../patches/wrf_bug_fixes/module_radiation_driver.F ./phys/
 cp ../../../patches/wrf_bug_fixes/module_cu_g3.F ./phys/
 
-# Fresh config
-./clean -a
-
 # input 21 (icc/ifort) and 1 (basic nesting) to configure script
 echo '20\n1\n' | ./configure
 
 # modifications for Intel 
 sed -i -e 's/-openmp/-qopenmp/' ./configure.wrf
-#sed -i -e 's/-lpnetcdf/-L${TACC_PNETCDF_LIB} -lnetcdff -lnetcdf /' ./configure.wrf
 sed -i -e 's/gcc/icc/' ./configure.wrf
 
-## NetCDF linking needed for Stampede2
+## Libs and includes modified in configure.wrf needed for Stampede2
 export var=" -DNETCDF -DPNETCDF -I${WRF_SRC_ROOT_DIR}/external/io_netcdf -I${NETCDF}/include -L${WRF_SRC_ROOT_DIR}/external/io_netcdf -lwrfio_nf -L${TACC_NETCDF_LIB} -lnetcdff -lnetcdf -I${WRF_SRC_ROOT_DIR}/external/io_pnetcdf -I${PNETCDF}/include -L${WRF_SRC_ROOT_DIR}/external/io_pnetcdf -lwrfio_pnf -L${TACC_PNETCDF_LIB} -lpnetcdf"
-#sed -i -e ''95's@$@'" $var"'@' configure.wrf
 awk -v mytext="${var}" '/^NETCDF4_DEP_LIB / {$0=$0mytext} 1' configure.wrf > temp && mv temp configure.wrf
 
 awk -v add=" -L${LIBS}/zlib-1.2.7" '/^ZLIB/ {$0=$0add} 1' configure.wrf > temp && mv temp configure.wrf
+awk -v add=" -L${TACC_HDF5_DIR}/lib -lhdf5_fortran -lhdf5" '/^HDF5/ {$0=$0add} 1' configure.wrf > temp && mv temp configure.wrf
 awk -v add="                      -I${WRF_SRC_ROOT_DIR}/external/io_pnetcdf \\" 'NR==200 {print add} {print}' configure.wrf > temp && mv temp configure.wrf
 awk -v add="                      -I${PNETCDF}/include \\" 'NR==201 {print add} {print}' configure.wrf > temp && mv temp configure.wrf
+awk -v add="                      -I${WRF_SRC_ROOT_DIR}/dyn_nmm \\" 'NR==202 {print add} {print}' configure.wrf > temp && mv temp configure.wrf
+awk -v add="                      -I${WRF_SRC_ROOT_DIR}/dyn_em \\" 'NR==203 {print add} {print}' configure.wrf > temp && mv temp configure.wrf
+
+export MODULEPATH=${WRF_SRC_ROOT_DIR}/dyn_nmm:${WRF_SRC_ROOT_DIR}/dyn_em:$MODULEPATH
+echo "MODULEPATH = ${MODULEPATH}"
 
 # Compile
 #time ./compile -j 2 em_real >& compile_em_real.log
